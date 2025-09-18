fc07d4c4d9f6f9a61ffa27f8a9030dea02e410ab
Leul Dessalegn <leuldessalegn@gmail.com>
2025-09-18 09:46:23 -0400

    The write stream works, and appends on the proper place. Had to change flag to R+ to append or it will create a null filled file for old items. Append works properly. Only appends if no duplicates exists.

--- DBtest.js
+++ DBtest.js
@@ -1,30 +1,49 @@
   const fs = require("node:fs");
   const fsasync = require('fs/promises');
   const pathlist = './users';
-  const FileWriteStream = require('./FileWriteStream')
+  const FileWriteStream = require('./FileWriteStream');
+  const {
+      json
+  } = require("node:stream/consumers");
 
-  const initializeData = () => {
-      this.query = fs.readFileSync(pathlist, "utf8")
-      this.list = undefined
-      if (this.query.trim()) { // If there's any content in the file
-          this.list = JSON.parse(this.query); // Parse the content into an array
-      }
-      return this.list
-  }
+
   //Loads data into memory
+  const initializeData = () => {
+      try {
+          this.query = fs.readFileSync(pathlist, "utf8")
+          this.list = undefined
+          if (this.query.trim()) { // If there's any content in the file
+              this.list = JSON.parse(this.query); // Parse the content into an array
+          }
+
+          return this.list
+      } catch (error) {
+          console.log('File Loading Error: ', error)
+      }
+  }
+
   const data = initializeData();
-  const buffer = Buffer.from(JSON.stringify(data), 'utf-8')
-  //NOTE when filtering for items in the database. Also, save the 
+  const buffer = Buffer.from(JSON.stringify(data))
+
+  const findItemLocation = (data) => {
+      let location = []
+      data.forEach(element => {
+          location.push([element, buffer.indexOf(Buffer.from(element))])
+      });
+      return location
+  }
+
   const filter = (searchKeys, matchingParameter) => {
       let result = data;
       console.log('searchkeys', searchKeys)
+      console.log('Matching Parameters: ', matchingParameter)
       for (const key in searchKeys) {
           result = result.filter(row => row[searchKeys[key]] == matchingParameter[searchKeys[key]])
-          console.log('RESULT: ', result)
       }
       console.log('LAST RUN Result: ', result)
       return result
   }
+
   const returnstruct = (data, struct, size = data.length) => {
       let returnfield = []
       for (const row in data) {
@@ -37,10 +56,35 @@
       return returnfield
   }
 
-  const query = (matchingParameter, returnstructure) => {
+  const returnAsList = (data) => {
+      let list = []
+      for (let d = 0; d < data.length; d++) {
+          let keys = Object.keys(data[d])
+          let values = Object.values(data[d])
+          for (let i = 0; i < keys.length; i++) {
+              if ((typeof values[i]) == "string") {
+                  list.push(`"${keys[i]}":"${values[i]}"`)
+              } else {
+                  list.push(`"${keys[i]}":${values[i]}`)
+              }
+
+          }
+      }
+      console.log('Return As List: ', list)
+      return list
+  }
+
+  const query = (matchingParameter, returnstructure, update) => {
       //Returns an array
       const searchKeys = Object.keys(matchingParameter)
       const filteredData = filter(searchKeys, matchingParameter)
+      const itemLocatioin = findItemLocation(returnAsList(filteredData))
+      if (update) {
+          return {
+              'itemlocation': itemLocatioin,
+              'result': returnstruct(filteredData, returnstructure)
+          }
+      }
       return returnstruct(filteredData, returnstructure)
 
   }
@@ -59,69 +103,66 @@
 
   }
 
-  const append = (newdata) => {
-      data.push(newdata)
-  }
-
-  //NOTE: The partdata should be an array of strings where each unit is key value pair
-  //Then each key value pair should looped through and changed in the database as it is found
-  //
-  const find = (partdata) => {
-      let keylength = 2;
-      const keyLength = partdata.forEach(element => {
-          let elements = Object.keys(element)
-          elements.push(Object.values(element))
-          console.log('Keys: ', elements)
-          keylength += elements.reduce((prev, curr) => prev + curr.length, 0)
-          this.values = Object.values(element)
-      })
-      //console.log('Key: ', Object.keys(element), '\n', "Value: ", values, '\n')
-      console.log("Size: ", keylength)
-      console.log('Search Parameter for Return Index: ', JSON.stringify(partdata).slice(1, partdata.length - 1))
-      console.log('DATA: ', JSON.stringify(data))
-      this.string = JSON.stringify(partdata)
-      //return buffer.indexOf(this.string.slice(1,this.string.length-1), 'utf-8')
-  }
-
-
-  const update = async (position, data, append) => {
-      try {
-          console.log(`Attempting to open file ${pathList}`)
-
-          var fileHandle = new FileWriteStream({
-              fileName: pathlist,
-              writableHighWaterMark: 0,
-              position: position,
-              append: append
-          })
-      } catch (error) {
-          console.log(error)
-          return error
-      } finally {
-          if (fileHandle != undefined) {
-              fileHandle.write(data, () => {
-                  console.log('written')
-              })
-              fileHandle.end()
-
-          }
+  const append = (record) => {
+      if (query(record).length > 0) {
+          return console.error('Error: Unable to append. Duplicate Record Found.');
+      } else {
+          data.push(record)
+          update(datalength(data), record)
+          console.log('Appended Data: ', data)
       }
   }
 
-  fileSize()
-  console.log("Executed Database: ", query({
-      username: "merit.sky",
+
+  //NOTE: The partdata should be an array of strings where each unit is key value pair
+  //Then each key value pair should looped through and changed in the database as it is found
+  const datalength = (data) => {
+      const keyLength = data.reduce((accumulator, element, index, array) => {
+          return accumulator + JSON.stringify(element).trim().length; // Accumulator is updated
+      }, 0);
+  }
+
+  const update = (position, data) => {
+      fileSize().then((size) => {
+          try {
+              console.log(`Attempting to open file ${pathlist}`)
+              var fileHandle = new FileWriteStream({
+                  fileName: pathlist,
+                  position: size - 1
+              })
+          } catch (error) {
+              console.log(error)
+              return error
+          } finally {
+              if (fileHandle != undefined) {
+                  fileHandle.write(Buffer.from(`,${JSON.stringify(data)}]`), () => {
+                      console.log('written')
+                  })
+                  fileHandle.end()
+
+              }
+          }
+      })
+  }
+
+  const queryresult = query({
+      username: "liam23",
       password: "string"
-  }, ['username', 'id']))
+  }, ['username', 'password'], true)
 
-  console.log(find([{
-      username: 'merit.sky',
-      id: 1
-  }]))
+   const appendresult = append({
+        id: 12,
+        name: "Mesfin Deb",
+        username: "mesfin.deb",
+        password: "TODO"
+    })
+    console.log('Append Result: ', appendresult) 
+  console.log("Executed Database: ", queryresult)
 
-  console.log('TEST',
-    [{
-      username: 'merit.sky',
-      id: 1
-  }].forEach(element => console.log('Real',JSON.stringify(element).trim().length))
-  )
+
+  let test = Buffer.from("HELLO")
+  let slice1 = test.slice(0, 2);
+  let newbuffer = Buffer.from("Hell")
+  let final = Buffer.concat([slice1, newbuffer, test])
+
+  console.log(final.toString())
